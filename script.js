52fcf93465a1aa3a668c08ab0816dfb4b8a8f6fe
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-05 14:24:59 +0200

    add validate and function to reverse string

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 //-------------------MAIN------------------------------------------
+//salvo in una variabile il div vuoto per far comparire il risultato a schermo dopo
+const userInputResult = document.getElementById("userResult");
+
+//faccio un prompt per chieder all'utente la parola
+const inputString = prompt("Inserisci una parola");
+//dichiaro la  funzione di validazione con parametro la stringa inserita dall'utente
+validateInputString(inputString);  
+
+//dichiaro la funzione che mi inverte la stringa e come parametro gli do l'input del prompt
+reverseString(inputString);
 
 
 
 
 
 
-
+console.log(reverseString(inputString));
 
 
 
@@ -25,3 +35,41 @@
 
 
 //----------------------FUNCTION-----------------------------------------------
+function reverseString(str){
+  // Step 1.  metodo split() per restituire un nuovo array
+  let splitString = str.split("");
+
+  // Step 2.  metodo reverse() per invertire l'array appena creato
+  let reverseArray = splitString.reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  
+  // Step 3.  metodo join() per unire tutti gli elementi della stringa in un array
+  let joinArray = reverseArray.join("")
+
+  
+  //Step 4. Restituisci la stringa invertita
+  return joinArray; // "olleh"
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+//funzione che dice fintanto che  l'input inserito dall'utente e' un numero oppure e' vuoto fai comparire il prompt 
+function validateInputString(variableString){
+    while(!isNaN(variableString) || variableString == ""){
+        variableString = prompt("non sono ammessi numeri e caratteri vuoti");
+    };
+
+   
+};
